4f8bf9e35dba7b2f5ec6ff1e41e31b1f7b4ddcbe
RinkalYadav <rinkalbyadav@gmail.com>
2026-03-30 11:05:08 +0530

    Perform Crud Operation

--- simpleWebApp/simpleWebApp/src/main/java/com/telusko/simpleWebApp/model/Product.java
+++ simpleWebApp/simpleWebApp/src/main/java/com/telusko/simpleWebApp/model/Product.java
@@ -2,10 +2,12 @@ package com.telusko.simpleWebApp.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private int prodId;
     private String prodName;
--- simpleWebApp/simpleWebApp/src/main/java/com/telusko/simpleWebApp/service/ProductService.java
+++ simpleWebApp/simpleWebApp/src/main/java/com/telusko/simpleWebApp/service/ProductService.java
@@ -1,16 +1,48 @@
 package com.telusko.simpleWebApp.service;
 
 import com.telusko.simpleWebApp.model.Product;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+@Getter
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(new Product(101,"IPhone",50000), new Product(102,"Camera",80000), new Product(103,"Laptop",90000));
+    List<Product> products = new ArrayList<>(Arrays.asList(new Product(101,"IPhone",50000), new Product(102,"Camera",80000), new Product(103,"Laptop",90000)));
 
     public List<Product> getProducts(){
         return products;
+    }
+
+    public Product getProductById(int prodId){
+        return products.stream()
+                .filter(p -> p.getProdId()==prodId)
+                .findFirst().orElse(new Product(100,"No item",0));
+    }
+
+    public void addProduct(Product prod){
+
+        products.add(prod);
+    }
+
+    public void updateProduct(Product prod){
+        int index=0;
+        for(int i=0;i<products.size();i++)
+            if(products.get(i).getProdId() == prod.getProdId())
+                index =i;
+
+        products.set(index,prod);
+    }
+
+    public void deleteProduct(int prodId){
+        int index=0;
+        for(int i=0;i<products.size();i++)
+            if(products.get(i).getProdId() == prodId)
+                index = i;
+
+        products.remove(index);
     }
 }
