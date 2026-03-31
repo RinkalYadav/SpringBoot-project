ec6e79f976a10262935080a30d5b0713fb794993
RinkalYadav <rinkalbyadav@gmail.com>
2026-03-31 09:52:14 +0530

    JPA Impmlimantation in my Project

---
+++ SimpleWebAppWithJpa/SimpleWebAppWithJpa/src/main/java/com/example/SimpleWebAppWithJpa/SimpleWebAppWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleWebAppWithJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebAppWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebAppWithJpaApplication.class, args);
+	}
+
+}
---
+++ SimpleWebAppWithJpa/SimpleWebAppWithJpa/src/test/java/com/example/SimpleWebAppWithJpa/SimpleWebAppWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleWebAppWithJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
